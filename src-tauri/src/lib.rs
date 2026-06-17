a5f59b59230c9635ea3814e347eeb8c86bbfb163
Johan Olvera <olvjohan2001@gmail.com>
2026-06-17 13:54:38 -0500

    added sqlx with migrations

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,49 @@
+mod commands;
+use std::fs::create_dir_all;
+
+use crate::commands::amortization_command::calculate_monthly_payment;
+use sqlx::sqlite::{SqlitePool, SqlitePoolOptions};
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-mod commands;
-
-use crate::commands::amortization_command::calculate_monthly_payment;
+pub struct AppState {
+    pub db: SqlitePool,
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let app_handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+                let db_path = app_handle
+                    .path()
+                    .app_data_dir()
+                    .expect("failed to resolve app data dir")
+                    .join("app.db");
+
+                create_dir_all(db_path.parent().unwrap()).unwrap();
+
+                let pool = SqlitePoolOptions::new()
+                    .max_connections(6)
+                    .connect(&format!("sqlite://{}?mode=rwc", db_path.display()))
+                    .await
+                    .expect("failed to connect to database");
+
+                sqlx::migrate!("./migrations")
+                    .run(&pool)
+                    .await
+                    .expect("failed to run migrations");
+
+                app_handle.manage(AppState { db: pool });
+            });
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet, calculate_monthly_payment])
         .run(tauri::generate_context!())
